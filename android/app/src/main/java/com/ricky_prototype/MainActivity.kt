acce2969513920773de6dffe76791391000cd331
fxricky <fanxuricky@gmail.com>
2024-08-03 22:49:47 +0800

    feat: add react-native-navigation

--- android/app/src/main/java/com/ricky_prototype/MainActivity.kt
+++ android/app/src/main/java/com/ricky_prototype/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.ricky_prototype
 
-import com.facebook.react.ReactActivity
-import com.facebook.react.ReactActivityDelegate
+// import com.facebook.react.ReactActivity
+import com.reactnativenavigation.NavigationActivity;
+// import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
-class MainActivity : ReactActivity() {
+class MainActivity : NavigationActivity() {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ricky_prototype"
+  // override fun getMainComponentName(): String = "ricky_prototype"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+  // override fun createReactActivityDelegate(): ReactActivityDelegate =
+  //     DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
--- index.js
+++ index.js
@@ -2,8 +2,26 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {Navigation} from 'react-native-navigation';
+// import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+Navigation.registerComponent('com.ricky_prototype', () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'com.ricky_prototype',
+            },
+          },
+        ],
+      },
+    },
+  });
+});
