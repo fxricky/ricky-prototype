48634d9b2816394a5e487905ad83d345721bc42f
fxricky <fanxuricky@gmail.com>
2024-08-04 23:43:06 +0800

    styles: add typography, color

---
+++ src/theme/color.ts
@@ -0,0 +1,26 @@
+export default Object.freeze({
+  PRIMARY: {
+    DARKEST: "#006ffd",
+    DARK: "#2897ff",
+    MEDIUM: "#6fbaff",
+    LIGHT: "#b4dbff",
+    LIGHTEST: "#eaf2ff",
+  },
+  NEUTRAL: {
+    LIGHT: {
+      DARKEST: "#C5C6CC",
+      DARK: "#D4D6DD",
+      MEDIUM: "#e8e9f1",
+      LIGHT: "#f8f9fe",
+      LIGHTEST: "#ffffff",
+    },
+    DARK: {
+      DARKEST: "#1f2024",
+      DARKEST_01: "rgba(31, 32, 36, 0.1)",
+      DARK: "#2f3036",
+      MEDIUM: "#494a50",
+      LIGHT: "#71727a",
+      LIGHTEST: "#8f9098",
+    },
+  },
+});
---
+++ src/theme/typography.ts
@@ -0,0 +1,5 @@
+export function typography(fontSize: number) {
+  return {
+    fontSize,
+  };
+}
